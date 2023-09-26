fec7679d2cf35ed7d5ca37e24f0729cb59026a7a
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-26 23:34:19 +0200

    Dynamic backgroud color

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,45 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { assets } from '@/utils/asset-utils';
+import { type Framework, frameworks } from '@/utils/framework-utils';
 
 export default component$(() => {
+  const currentFramework = useSignal<Framework>(frameworks[0]);
+
+  useVisibleTask$(({ cleanup }) => {
+    let currentIndex: number = 0;
+
+    const rotateFrameworks = () => {
+      currentFramework.value = frameworks[currentIndex];
+      currentIndex = (currentIndex + 1) % frameworks.length;
+    };
+
+    const intervalId = setInterval(rotateFrameworks, 2000);
+
+    cleanup(() => clearInterval(intervalId));
+  });
+
   return (
     <main>
+      <div
+        class={[
+          'fixed',
+          'inset-0',
+          'transition-colors',
+          'delay-100',
+          'duration-700',
+          'opacity-25',
+          { 'bg-purple-300': currentFramework.value === 'qwik' },
+          { 'bg-sky-300': currentFramework.value === 'safari' },
+          { 'bg-yellow-300': currentFramework.value === 'chrome' },
+          { 'bg-teal-300': currentFramework.value === 'tailwind' },
+          { 'bg-blue-300': currentFramework.value === 'react' },
+          { 'bg-green-300': currentFramework.value === 'vue' },
+          { 'bg-orange-300': currentFramework.value === 'svelte' },
+          { 'bg-red-300': currentFramework.value === 'mobile' },
+          { 'bg-neutral-300': currentFramework.value === 'desktop' },
+        ]}
+      />
       <img
         src={assets.gradient}
         width={1200}
