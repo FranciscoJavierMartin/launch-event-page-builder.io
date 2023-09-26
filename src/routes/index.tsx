6329c1ce092dfcd66282b9214cfaf83e0df80a47
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-26 23:18:18 +0200

    Add background image

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,27 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { assets } from '@/utils/asset-utils';
 
 export default component$(() => {
-  return <></>;
+  return (
+    <main>
+      <img
+        src={assets.gradient}
+        width={1200}
+        height={1200}
+        role='presentation'
+        alt='gradient background'
+        class='fixed inset-0 w-screen h-screen object-cover'
+      />
+      <div
+        class='fixed inset-0 opacity-30'
+        style={{
+          backgroundSize: '30px',
+          backgroundImage: `url(${assets.square})`,
+        }}
+      ></div>
+    </main>
+  );
 });
 
 export const head: DocumentHead = {
