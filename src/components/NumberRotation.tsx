90d45ead4279f5404c2446083097457304659dc5
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-27 18:28:43 +0200

    Rotate number

--- src/components/NumberRotation.tsx
+++ src/components/NumberRotation.tsx
@@ -1,8 +1,27 @@
-import { component$ } from "@builder.io/qwik";
+import { component$ } from '@builder.io/qwik';
 
-export default component$<{value:number}>(({value}) => {
+export default component$<{ value: number }>(({ value }) => {
+  const values = Array.from({ length: 60 }, (_, i) => i);
 
-  return(
-    <div class='relative h-10 w-10'></div>
-  )
-})
+  return (
+    <div class='relative h-10 w-10'>
+      {values.map((num) => (
+        <div
+          key={num}
+          class={[
+            'w-full',
+            'h-full',
+            'absolute',
+            'transition-all',
+            'duration-200',
+            { 'opacity-100 transform-none': value === num },
+            { 'opacity-0 -translate-y-2': value > num },
+            { 'opacity-0 translate-y-2': value < num },
+          ]}
+        >
+          {num}
+        </div>
+      ))}
+    </div>
+  );
+});
